e3cc2866ea234cb489625e58d231cbbfdf098419
dheavin786 <100607931+dheavin786@users.noreply.github.com>
2022-05-26 16:52:11 +0100

    Update pyCSVReader.py

--- pyCSVReader.py
+++ pyCSVReader.py
@@ -5,7 +5,7 @@ from qpython.qcollection import qlist
 from qpython.qtype import QException, QTIME_LIST, QSYMBOL_LIST, QFLOAT_LIST, QLONG_LIST
 
 with qconnection.QConnection(host='kxtraining', port=6000) as q:
-    with open("C:/Users/mmcalister/Downloads/trade.csv", "r") as f:
+    with open("C:/Users/dinah/Downloads/trade.csv", "r") as f:
         reader = csv.reader(f, delimiter=",")
         for line in enumerate(reader):
             if (line[1][0] == 'time'):
