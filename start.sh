e03130c10bce31910568e4bf8177cfd4b6c97fc9
dheavin786 <100607931+dheavin786@users.noreply.github.com>
2022-05-28 14:18:35 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 #nohup q tick.q sym . -p 5011 1>>tick.log 2>&1 &
-#nohup q tick/feed.q 1>>feed.log 2>&1 &
-#nohup q tick/rTQ.q localhost:5011 1>>newestTQ2.log 2>&1 &
+#nohup q tick/feedhandler.q 1>>feed.log 2>&1 &
+#nohup q tick/rdbTQ.q localhost:5011 1>>newestTQ2.log 2>&1 &
 #nohup q tick/cep.q localhost:5011 1>>cep.log 2>&1 &
-#nohup q tick/rAGG.q localhost:5011 1>>rAGG.log 2>&1 &
+#nohup q tick/rdbAGG.q localhost:5011 1>>rAGG.log 2>&1 &
 
 
 source ./config.sh
@@ -39,5 +39,5 @@ echo "Enter y to run the Mock Feedhandler (y/n)?"
 read feedbool
 
 if [ "$feedbool" = "y" ]; then
-        nohup $q $advancedKDB/tick/feed.q 1>>$advancedKDB/logs/feed.log 2>&1 &
+        nohup $q $advancedKDB/tick/feedhandler.q 1>>$advancedKDB/logs/feed.log 2>&1 &
 fi
