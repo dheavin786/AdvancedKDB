781c27c5fb39059b503d4acdef4ae4e201ad8998
dheavin786 <100607931+dheavin786@users.noreply.github.com>
2022-05-28 14:13:37 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export dir=${PWD}
-export advancedKDB="$(dirname "$dir")"
+export advancedKDB="$(/home/local/FD/dheavin/AdvancedKDB)"
 export Q=$QHOME
 export q=$QHOME"q"
 export tpPort=6001
