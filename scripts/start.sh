626fcb59f8fb72dada413094b209d50e25611ff9
dheavin786 <100607931+dheavin786@users.noreply.github.com>
2022-08-04 20:15:03 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,33 +11,33 @@ echo "Enter y to run the Tickerplant, (y/n)"
 read tickbool
 
 if [ "$tickbool" = "y" ]; then
-        nohup $q $advancedKDB/tick.q sym . -p $tpPort 1>>$advancedKDB/logs/tick.log 2>&1 &
+        nohup $q $AdvancedKDB/tick.q sym . -p $tpPort 1>>$AdvancedKDB/logs/tick.log 2>&1 &
 fi
 
 echo "Enter y to run the Trade/Quote RDB (y/n)?"
 read rTQbool
 
 if [ "$rTQbool" = "y" ]; then
-    nohup $q $advancedKDB/tick/rTQ.q localhost:$tpPort 1>>$advancedKDB/logs/rTQ.log 2>&1 &
+    nohup $q $AdvancedKDB/tick/rTQ.q localhost:$tpPort 1>>$AdvancedKDB/logs/rTQ.log 2>&1 &
 fi
 
 echo "Enter y to run the Aggregation RDB (y/n)?"
 read rAGGbool
 
 if [ "$rAGGbool" = "y" ]; then
-        nohup $q $advancedKDB/tick/rAGG.q localhost:$tpPort 1>>$advancedKDB/logs/rAGG.log 2>&1 &
+        nohup $q $AdvancedKDB/tick/rAGG.q localhost:$tpPort 1>>$AdvancedKDB/logs/rAGG.log 2>&1 &
 fi
 
 echo "Enter y to run the Start CEP (y/n)?"
 read cepbool
 
 if [ "$cepbool" = "y" ]; then
-    nohup $q $advancedKDB/tick/cep.q localhost:$tpPort 1>>$advancedKDB/logs/cep.log 2>&1 &
+    nohup $q $AdvancedKDB/tick/cep.q localhost:$tpPort 1>>$AdvancedKDB/logs/cep.log 2>&1 &
 fi
 
 echo "Enter y to run the Mock Feedhandler (y/n)?"
 read feedbool
 
 if [ "$feedbool" = "y" ]; then
-        nohup $q $advancedKDB/tick/feed.q 1>>$advancedKDB/logs/feed.log 2>&1 &
+        nohup $q $AdvancedKDB/tick/feed.q 1>>$AdvancedKDB/logs/feed.log 2>&1 &
 fi
